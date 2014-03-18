3fbd1b4d4efaa7e2702ce64c18d739a4ff6d8d1b
Bryan Jensen <bryanawjensen@gmail.com>
2014-03-18 13:33:58 -0400

    Got social links working with webfont icons

--- web.js
+++ web.js
@@ -27,6 +27,7 @@ app.configure(function() {
 	app.use('/css', express.static(__dirname + '/css'));
 	app.use('/js', express.static(__dirname + '/js'));
     app.use('/images', express.static(__dirname + '/images'));
+    app.use('/static', express.static(__dirname + '/static'));
 });
 
 var port = 7500;
