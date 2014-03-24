2be00d67eb81e9d909f964c83093fe369b6afc74
Bryan Jensen <bryanawjensen@gmail.com>
2014-03-24 13:56:27 -0400

    Working on firebase filling out of competition page

--- js/script.js
+++ js/script.js
@@ -9,4 +9,26 @@ function centerLogo() {
 
 $(function() {
 	centerLogo();
+
+	if ($('.competition').length) {
+		var compsRef = new Firebase('https://whalesite.firebaseio.com/Competitions');
+
+		compsRef.on('child_added', function (snapshot) {
+			var entry = snapshot.val();
+			var template = $.get(window.location.host + '/static/templates/competition.html');
+
+			// console.dir(entry.Entrants);
+
+			$.each(entry.Entrants, function() {
+				// console.dir(this.name);
+				if (this.name != '') {
+					console.log(this.name);
+				}
+				// if (typeof(this) == typeof(Object)) {
+					// console.dir(this);
+				// }
+				// console.dir(this);
+			});
+		});
+	}
 });
