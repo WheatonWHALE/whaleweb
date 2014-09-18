da36b3dd5cee840115ebf95db709f953622532ed
Bryan Jensen <bryanawjensen@gmail.com>
2014-09-17 23:16:23 -0400

    Playing around with resizing thsoe without streaks

--- js/script.js
+++ js/script.js
@@ -27,10 +27,26 @@ function sortByMaxStreak(a, b) {
 }
 
 function markTopThree(element) {
+	/*
+	Function to mark the top three people as being in the lead. Used in styling them differently.
+	*/
+
 	// Note: 2, 3, 4 because the first child is the title
 	element.find('.entrant:nth-child(2)').addClass('leader').addClass('first');
 	element.find('.entrant:nth-child(3)').addClass('leader').addClass('second');
 	element.find('.entrant:nth-child(4)').addClass('leader').addClass('third');
+}
+
+function markStreakless(element) {
+	/*
+	Function to mark everyone without an active streak.
+	*/
+	element.find('.score').each(function() {
+		var that = $(this);
+
+		if (that.html() == '0 days')
+			that.parents('.entrant').addClass('streakless');
+	});
 }
 
 function centerLogo() {
@@ -58,19 +74,21 @@ $(function() {
 				entrants.push(this);
 			});
 
-			entrants.sort(sortByCurrentStreak)
+			entrants.sort(sortByCurrentStreak);
 
 			dust.render("current_streak", {title: 'Current Streak', entrants: entrants}, function(err, out) {
 				out = $(out);
 				markTopThree(out);
+				markStreakless(out);
 				$("#competition-container").append(out);
 			});
 
-			entrants.sort(sortByMaxStreak)
+			entrants.sort(sortByMaxStreak);
 
 			dust.render("max_streak", {title: 'Max Streak', entrants: entrants}, function(err, out) {
 				out = $(out);
 				markTopThree(out);
+				markStreakless(out);
 				$("#competition-container").append(out);
 			});
 
