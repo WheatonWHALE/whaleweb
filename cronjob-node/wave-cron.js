5672a6af3ddba7fa10f0239436f17935810df433
Bryan Jensen <bryanawjensen@gmail.com>
2014-09-28 13:25:05 -0400

    Cleaning up extra data sent, and starting parse

--- cronjob-node/wave-cron.js
+++ cronjob-node/wave-cron.js
@@ -29,36 +29,29 @@ var cheerio = require("cheerio"),
 
 // waitForFinish();
 
+function parseOutData(body) {
+    $ = cheerio.load(body);
+
+    $('.tr');
+}
+
 function fetchData() {
     var url = 'https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_OpenDoor';
 
     var dataValues = {
         'intmajor_sch' : '%',
         'area_sch' : '%',
-        'area_cat' : '%',
         'submit_btn' : 'Search Schedule',
         'subject_sch' : '%',
-        'subject_cat' : '%',
         'foundation_sch' : '%',
         'schedule_beginterm' : '201510',
-        'intmajor_cat' : '%',
         'division_sch' : '%',
-        'foundation_cat' : '%',
         'crse_numb' : '%',
-        'division_cat' : '%'
     };
 
-    var headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36',
-        'Cookie': '_ga=GA1.2.99013180.1410794952; session=eyJyZWNlbnQiOlsiMTNxMHdjcDEiXX0.BviaPw.QJ5xAWB91r8I21WDad6uYYk6xPw'
-    };
-
-    request.post(url, {form:dataValues, headers:headers}, function(err, resp, body) {
-        console.log('test');
-        console.log(resp);
-
-        $ = cheerio.load(body);
-
-        $('.tr')
+    request.post(url, {form:dataValues}, function(err, resp, body) {
+        console.log(body);
     });
 }
+
+fetchData();
