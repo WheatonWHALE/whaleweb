688b51a8760274af18cd98e1d08681d2c6d9b868
Bryan Jensen <bryanawjensen@gmail.com>
2014-09-28 16:40:26 -0400

    In progress commit - issues with year and async stuff

--- cronjob-node/wave-cron.js
+++ cronjob-node/wave-cron.js
@@ -29,16 +29,75 @@ var cheerio = require("cheerio"),
 
 // waitForFinish();
 
-function parseOutData(body) {
+// Note: Fall 2014 is considered part of the 2015 year
+// Years: 2004 (Fall 2013) - 2016 (Spring 2016)
+var startYear = 2015;
+var endYear = 2015;
+
+var possibleSemesters = [
+    '10',   // Fall
+    '15',   // Winter
+    '20',   // Spring
+    '35'    // Summer
+]
+
+function parseClassData(allRows, i, year, semester) {
+    // 2 Types of formats:
+    //      W/o prereqs: 5 lines long, info on lines 0,    2
+    //      Prereqs:     6 lines long, info on lines 0, 1, 3
+
+    var firstRow  = $(allRows[i]);
+    var secondRow = $(allRows[i+1]);
+    var thirdRow  = $(allRows[i+2]);
+    var fourthRow = $(allRows[i+3]);
+
+    var enrollmentRow;
+
+    if (thirdRow.find('td').length > 3)
+        classHasPrereqs = false;
+    else
+        classHasPrereqs = true;
+
+    if (classHasPrereqs)
+        enrollmentRow = thirdRow;
+    else
+        enrollmentRow = fourthRow;
+
+
+    firstRowElements      = firstRow.find('td');
+    enrollmentRowElements = enrollmentRow.find('td');
+    // Testing for prereqs row
+
+    classObject = {
+        classCode: $(firstRowElements[0]).text(),
+        examSlot: $(firstRowElements[1]).text()
+    };
+
+    // classObject.each()
+    for (var key in classObject) {
+        classObject[key] = classObject[key].replace(/\n/g, '');
+    }
+
+    console.log(classObject);
+
+    console.log(year + ' : ' + semester);
+
+    process.exit(0);
+}
+
+function parseData(body, year, semester) {
     $ = cheerio.load(body);
 
     var classLabelMatch = /[A-Z][A-Z][A-Z][A-Z]?-[0-9][0-9][0-9]/;
 
-    allRows = $('.tr');
+    allRows = $('tr');
 
     allRows.each(function(index, element) {
-        // element.text()
-        console.log($(this).text());
+        var possibleClassLabel = $(this).find('td').text();
+
+        if (possibleClassLabel.match(classLabelMatch)) {
+            parseClassData(allRows, index, year, semester);
+        }
     });
 }
 
@@ -51,14 +110,30 @@ function fetchData() {
         'submit_btn' : 'Search Schedule',
         'subject_sch' : '%',
         'foundation_sch' : '%',
-        'schedule_beginterm' : '201510',
+        'schedule_beginterm' : '',
         'division_sch' : '%',
         'crse_numb' : '%',
     };
 
-    request.post(url, {form:dataValues}, function(err, resp, body) {
-        console.log(body);
-    });
+    var year;
+    var semester;
+
+    for (year = startYear; year <= endYear; year++) {
+        possibleSemesters.forEach(function(entry, index, array) {
+            semester = entry;
+
+            dataValues['schedule_beginterm'] = year.toString() + semester;
+
+            console.log(dataValues);
+
+            request.post(url, {form:dataValues}, function(err, resp, body) {
+                if (resp.statusCode == 200)
+                    parseData(body, year, semester);
+                else
+                    console.log(resp.statusCode);
+            });
+        });
+    }
 }
 
 fetchData();
