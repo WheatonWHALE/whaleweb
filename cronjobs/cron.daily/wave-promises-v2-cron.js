9c59e7e07f342f69f3e948bea60ede871cce204c
Bryan Jensen <bryanawjensen@gmail.com>
2014-10-13 19:41:06 -0400

    Set up for prettification step, but aborting

--- cronjobs/cron.daily/wave-promises-v2-cron.js
+++ cronjobs/cron.daily/wave-promises-v2-cron.js
@@ -1,5 +1,9 @@
-var request = require('request')
-    cheerio = require('cheerio');
+var request = require('request'),
+    cheerio = require('cheerio'),
+    fs      = require('fs'),
+    jade    = require('jade');
+
+var debug = true;
 
 // Order of events:
 // 1. Parse 'search page' to get all the filters (departments, areas, etc.)
@@ -43,10 +47,83 @@ function get(url) {
     });
 }
 
-var filterTranslator;
+var filterValueTranslator;
+function prettifyFilterValue(filterValue) {
+    filterValueTranslator = filterValueTranslator ||
+    {
+        // Departments
+        'AFDS': '',
+        'ANTH': '',
+        'ARBC': '',
+        'ARTH': '',
+        'ARTS': '',
+        'AST': '',
+        'BIO': '',
+        'MGMT': '',
+        'CHEM': '',
+        'CHIN': '',
+        'CLAS': '',
+        'COMP': '',
+        'CONX': '',
+        'CW': '',
+        'ECON': '',
+        'EDUC': '',
+        'ENG': '',
+        'ENV': '',
+        'FNMS': '',
+        'FSEM': '',
+        'FR': '',
+        'GER': '',
+        'GK': '',
+        'HISP': '',
+        'HIST': '',
+        'INT': '',
+        'IR': '',
+        'ITAS': '',
+        'JAPN': '',
+        'LAT': '',
+        'MATH': '',
+        'MUSC': '',
+        'MUSP': '',
+        'NEUR': '',
+        'PHIL': '',
+        'PHYS': '',
+        'POLS': '',
+        'PSY': '',
+        'PH': '',
+        'REL': '',
+        'RUSS': '',
+        'SOC': '',
+        'THEA': '',
+        'URB': '',
+        'WGS': '',
+        'WMST': '',
 
-function prettifyFilter(filterName) {
-    filterTranslator = filterTranslator ||
+        // Foundations
+        'BW': '',
+        'FS': '',
+        'WR': '',
+        'FL': '',
+        'QA': '',
+
+        // Divisions
+        'DVAH': '',
+        'DVNS': '',
+        'DVSS': '',
+
+        // Areas
+        'ARCA': '',
+        'ARHS': '',
+        'ARHM': '',
+        'ARMC': '',
+        'ARNS': '',
+        'ARSS': ''
+    }
+}
+
+var filterClassTranslator;
+function prettifyFilterClass(filterClassName) {
+    filterClassTranslator = filterClassTranslator ||
     {
         'subject_sch': 'department',
         'foundation_sch': 'foundation',
@@ -56,9 +133,9 @@ function prettifyFilter(filterName) {
         // 'intmajor_sch': 'interdis_major' // Currently don't care about interdisciplinary majors
     }
 
-    var translated = filterTranslator[filterName];
+    var translated = filterClassTranslator[filterClassName];
     if (translated == undefined) {
-        translated = filterName;
+        translated = filterClassName;
     }
 
     return translated;
@@ -71,29 +148,77 @@ function fetchSearchPage() {
 function parseOutFilters(searchPageBody) {
     $ = cheerio.load(searchPageBody);
 
-    var filtersObject = {};
+    var filterObj = {};
 
     filters.forEach(function(filter, i, array) {
-        var prettifiedFilter = prettifyFilter(filter);
-        filtersObject[prettifiedFilter] = [];
+        var prettifiedFilter = prettifyFilterClass(filter);
+        filterObj[prettifiedFilter] = [];
 
         $('select[name=' + filter + ']').find('option').each(function(entry) {
             var filterValue = $(this).val();
             if (filterValue != '%')
-                filtersObject[prettifiedFilter].push({ val: filterValue, display: filterValue });
+                filterObj[prettifiedFilter].push({ val: filterValue, display: prettifyFilterValue(filterValue) });
         });
     });
 
-    return filtersObject;
+    return filterObj;
+}
+
+function saveFilters(filterObj) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.readFile('static/course-data/filters.jade', function(err, data) {
+            if (err)
+                reject(err);
+            else {
+                console.log(filterObj);
+                var func = jade.compile(data, { pretty: debug });
+                var html = func({ filterData: filterObj });
+
+                resolve(html);
+            }
+        })
+    }).then(function(html) {
+        // console.log('into saving filters');
+        fs.writeFile('static/course-data/compiled/filters.html', html, function(err) {
+            if (err) console.error(err);
+            else console.log('The filters html file was saved');
+        });
+
+        if (debug) {
+            fs.writeFile('static/course-data/filters.json', JSON.stringify(filterObj, null, 2), function(err) {
+                if (err) console.error(err);
+                else console.log("The filters json file was saved!");
+            });
+        }
+    });
 }
 
 function getSearchFilters() {
-
-    fetchSearchPage().then(parseOutFilters)
-    .catch(function(err) {
+    return fetchSearchPage().then(parseOutFilters).catch(function(err) {
+        console.error('getSearchFilters errored:');
         console.error(err);
         throw err;
     });
 }
 
-getSearchFilters();
+function preprocessFilters(rawFilterObj) {
+
+}
+
+function getScheduleData(semesters) {
+
+}
+
+function fetchAndParseAll() {
+    getSearchFilters()
+    .then(preprocessFilters)
+    .then(function(filterObj) {
+        console.log('Filters: ');
+        console.log(filterObj);
+        saveFilters(filterObj);
+
+        return getScheduleData(filterObj['semester']);
+    });
+}
+
+fetchAndParseAll();
