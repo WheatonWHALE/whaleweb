6c9fd20b2b6a462b74072fbe53f3cfa4f6b18a0a
Bryan Jensen <bryanawjensen@gmail.com>
2014-10-13 21:53:38 -0400

    Removing old crons, replacing main with new promise-based cron

--- cronjobs/cron.daily/wave-cron.js
+++ cronjobs/cron.daily/wave-cron.js
@@ -1,9 +1,9 @@
-var debug = true;
+var request = require('request'),
+    cheerio = require('cheerio'),
+    fs      = require('fs'),
+    jade    = require('jade');
 
-var cheerio = require("cheerio"),
-    request = require("request"),
-    fs      = require("fs"),
-    jade    = require("jade");
+var debug = false;
 
 // Adding a method to arrays to 'clean' out unwanted values
 Array.prototype.clean = function(deleteValue) {
@@ -16,18 +16,182 @@ Array.prototype.clean = function(deleteValue) {
     return this;
 };
 
-// Note: Fall 2014 is considered part of the 2015 year
-// Years: 2004 (Fall 2003) - 2016 (Spring 2016)
-var startYear = 2014;   // Inclusive
-var endYear = 2015;     // Inclusive
+// Order of events:
+// 1. Parse 'search page' to get all the filters (departments, areas, etc.)
+// 	  and most importantly, get possible semesters
+// 2. Query using those semesters, to build a database. Set up translation
+//    to turn cryptic stuff from existing schedule into most understandable
+//    terms. For instance, instead of '201510' (a.k.a. Fall 2014) it would be
+//    an object with { '2014': { 'fall': data } }, etc.
+// 3. Make second pass over the data, to perform the conversion of ugly data
+//    into nice, readable stuff, as a static pre-process step
+// 4. Save the filters and the course data into html files to be included in
+//    in the jade files.
 
-// All the possible semesters
-var possibleSemesters = [
-    10,   // Fall
-    15,   // Winter
-    20,   // Spring
-    35    // Summer
-]
+
+// Things to keep in mind:
+// - Async stuff should be preferably be promises, not callbacks
+// - Not everything is or should be async
+// - All network stuff (requests) should be async
+// - This is fairly sequential, so overall structure can probably not be async
+
+// =========================== General Stuff ==========================
+
+function get(url) {
+    // Return a new promise.
+    return new Promise(function(resolve, reject) {
+        request.get(url, function(err, resp, body) {
+            if (err)
+                return reject(err);
+            else
+                return resolve(body);
+        });
+    });
+}
+
+// Slightly specialized for use in posting for semester data
+function semesterPost(url, formData) {
+    // Have to perform a deep copy for the variable to not be overwritten by the time the callback is used.
+    var copiedFormData = JSON.parse(JSON.stringify(formData));
+
+    return new Promise(function(resolve, reject) {
+        console.log('Posting for ' + copiedFormData['schedule_beginterm']);
+        request.post(url, { form: copiedFormData }, function(err, resp, body) {
+            if (err)
+                return reject(err);
+            else
+                return resolve({ code: copiedFormData['schedule_beginterm'], body: body });
+        });
+    });
+}
+
+function formatConvertYear(intYear) {
+    return (intYear - 1).toString() + '-' + intYear.toString();
+}
+
+
+// =========================== Filter Stuff ===========================
+
+// All the filters we care about.
+var filters = [
+    'subject_sch',
+    'foundation_sch',
+    'division_sch',
+    'area_sch',
+    'schedule_beginterm'
+    // 'intmajor_sch' // Currently don't care about interdisciplinary majors
+];
+
+function prettifyFilterValue(filterValueText) {
+    // Possibly inefficient due to unnecessary replaces
+    return filterValueText.trim()
+        .replace(/Found: /, '')
+        .replace(/Area: /, '')
+        .replace(/Division: /, '');
+}
+
+var filterTranslator;
+function prettifyFilter(filterName) {
+    filterTranslator = filterTranslator ||
+    {
+        'subject_sch': 'department',
+        'foundation_sch': 'foundation',
+        'division_sch': 'division',
+        'area_sch': 'area',
+        'schedule_beginterm': 'semester'
+        // 'intmajor_sch': 'interdis_major' // Currently don't care about interdisciplinary majors
+    }
+
+    var translated = filterTranslator[filterName];
+    if (translated == undefined) {
+        translated = filterName;
+    }
+
+    return translated;
+}
+
+function fetchSearchPage() {
+    return get('https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_ListSection');
+}
+
+function parseOutFilters(searchPageBody) {
+    $ = cheerio.load(searchPageBody);
+
+    var filterObj = {};
+
+    filters.forEach(function(filter, i, array) {
+        var prettifiedFilter = prettifyFilter(filter);
+        filterObj[prettifiedFilter] = [];
+
+        $('select[name=' + filter + ']').find('option').each(function(entry) {
+            var filterValue = $(this).val();
+            if (filterValue != '%')
+                filterObj[prettifiedFilter].push({ val: filterValue, display: prettifyFilterValue($(this).text()) });
+        });
+    });
+
+    return filterObj;
+}
+
+function saveFilters(filterObj) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.readFile('static/course-data/filters.jade', function(err, data) {
+            if (err)
+                reject(err);
+            else {
+                var func = jade.compile(data, { pretty: debug });
+                var html = func({ filterData: filterObj });
+                resolve(html);
+            }
+        })
+    }).then(function(html) {
+        fs.writeFile('static/course-data/compiled/filters.html', html, function(err) {
+            if (err) console.error(err);
+            else console.log('The filters html file was saved!');
+        });
+
+        if (debug) {
+            fs.writeFile('static/course-data/filters.json', JSON.stringify(filterObj, null, 2), function(err) {
+                if (err) console.error(err);
+                else console.log("The filters json file was saved!");
+            });
+        }
+    });
+}
+
+function getSearchFilters() {
+    return fetchSearchPage().then(parseOutFilters).catch(function(err) {
+        console.error(err);
+        throw err;
+    });
+}
+
+function preprocessFilters(filterObj) {
+    // Preprocess to add the 'years' to the object, as they're something extra
+    // not inherent in the old course schedule
+    var integerSemesterCodes = filterObj['semester'].map(function(entry, i, array) {
+        return parseInt(entry.val);
+    });
+
+    filterObj.year = [];
+
+    // Floor the result of the integer code (in the format YYYYSS, for Year and Semester)
+    // divided by 100, to isolate the year.
+    var latestYear = Math.floor(Math.max.apply(null, integerSemesterCodes) / 100);
+    var earliestYear = Math.floor(Math.min.apply(null, integerSemesterCodes) / 100);
+
+    for (var currYear = latestYear; currYear >= earliestYear; --currYear) {
+        var yearFilterValue = formatConvertYear(currYear);
+        var yearFilterDisplay = 'Fall ' + (currYear - 1).toString() + ' - Spring ' + currYear.toString();
+        filterObj.year.push({ val: yearFilterValue, display: yearFilterDisplay });
+    }
+
+    return filterObj;
+}
+
+// =========================== Course Stuff ===========================
+
+var scheduleData = {};
 
 // Translates the number system that Wheaton uses into the word system that WAVE uses
 var semesterTranslator = {
@@ -36,14 +200,22 @@ var semesterTranslator = {
     20: 'spring',
     35: 'summer'
 }
+function extractInfoFromCode(semesterCode) {
+    var integerCode = parseInt(semesterCode);
+    var returnObj = { year: formatConvertYear(Math.floor(integerCode / 100)), semester: semesterTranslator[integerCode % 100] };
 
-// Translates the year from an int (e.g. 2014) to a string (e.g. '2013-2014')
-function translateYear(year) {
-    return (year - 1).toString() + '-' + year.toString();
+    return returnObj;
 }
 
-// Global variable for schedule data
-var scheduleData = {};
+function getSemesterPages(semesters) {
+    return Promise.all(
+        semesters.map(function(value, i, array) {
+            var tempFormData = dataValues;
+            tempFormData['schedule_beginterm'] = value.val;
+            return semesterPost('https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_OpenDoor', tempFormData);
+        })
+    );
+}
 
 // Parses one course's data out from the given list of rows
 function parseCourseData(allRows, i) {
@@ -67,7 +239,6 @@ function parseCourseData(allRows, i) {
         enrollmentRow = thirdRow;
     else
         enrollmentRow = fourthRow;
-
 
     firstRowElements      = firstRow.find('td');
     enrollmentRowElements = enrollmentRow.find('td');
@@ -99,23 +270,20 @@ function parseCourseData(allRows, i) {
             courseData[key] = courseData[key].trim()/*.replace(/\n/g, '')*/;
         }
         catch (err) {
-            console.log(err);
-            console.log(courseData);
-            console.log(key);
+            console.error(err);
         }
     }
 
     return courseData;
 }
 
-// Parses the semester data out from the given html
-function parseSemesterData(body) {
+function parseOutSemesterData(body) {
     $ = cheerio.load(body);
 
     var semesterCourses = {};
     var classLabelMatch = /[A-Z][A-Z][A-Z][A-Z]?\-[0-9][0-9][0-9]/;
 
-    allRows = $('tr');
+    var allRows = $('tr')
 
     allRows.each(function(index, element) {
         var possibleClassLabel = $(this).find('td').text().trim();
@@ -134,155 +302,96 @@ function parseSemesterData(body) {
     return semesterCourses;
 }
 
-// Writes all the schedule data to a file
-function writeDataToFile(schData) {
-    for (var currYear = startYear; currYear <= endYear; currYear++) {
-        var translatedYear = translateYear(currYear);
+var dataValues = {
+    'intmajor_sch' : '%',
+    'area_sch' : '%',
+    'submit_btn' : 'Search Schedule',
+    'subject_sch' : '%',
+    'foundation_sch' : '%',
+    'schedule_beginterm' : '', // Nothing in this one yet
+    'division_sch' : '%',
+    'crse_numb' : '%',
+};
+function getScheduleData(semesters) {
+    return getSemesterPages(semesters).then(function(rawSemesterDataArray) {
+        rawSemesterDataArray.forEach(function(semester, i, array) {
+            console.log('Parsing for ' + semester.code);
+            var semesterInfo = extractInfoFromCode(semester.code);
 
-        fs.readFile('static/course-data/courses.jade', function(err, data) {
-            if (err) console.log(err);
+            if (!(semesterInfo.year in scheduleData))
+                scheduleData[semesterInfo.year] = {};
 
-            var func = jade.compile(data, { pretty: debug });
-            var html = func({ courseData: schData[translatedYear] });
-
-            fs.writeFile('static/course-data/compiled/' + translatedYear + '.html', html, function(err) {
-                if (err) console.log(err);
-                else console.log("The courses " + translatedYear + " html file was saved!");
-            });
-
-            if (debug) {
-                fs.writeFile('static/course-data/' + translatedYear + '.json', JSON.stringify(schData[translatedYear], null, 2), function(err) {
-                    if (err) console.log(err);
-                    else console.log("The courses " + translatedYear + " json file was saved!");
-                });
-            }
+            scheduleData[semesterInfo.year][semesterInfo.semester] = parseOutSemesterData(semester.body);
         });
-    }
-}
-
-// Add the currently supported years to the filters object
-function addYearsToFilters(filtersObject) {
-    filtersObject['years'] = [];
-    for (var currYear = startYear; currYear <= endYear; currYear++) {
-        var translatedYear = translateYear(currYear);
-        filtersObject['years'].push({ val: translatedYear, display: translatedYear });
-    }
-}
-
-// Function that does everything to do with the acutal schedule and course data, which is almost all the work
-function fetchAndParseScheduleData() {
-    var url = 'https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_OpenDoor';
-
-    var dataValues = {
-        'intmajor_sch' : '%',
-        'area_sch' : '%',
-        'submit_btn' : 'Search Schedule',
-        'subject_sch' : '%',
-        'foundation_sch' : '%',
-        'schedule_beginterm' : '', // Nothing in this one yet
-        'division_sch' : '%',
-        'crse_numb' : '%',
-    };
-
-    var numResponses = 0;
-    var numResponsesExpected = possibleSemesters.length * (endYear+1 - startYear);
-
-    // Two different types of for loops for the different types of data
-    for (var currYear = startYear; currYear <= endYear; currYear++) {
-        var translatedCurrYear = translateYear(currYear);
-        scheduleData[translatedCurrYear] = {};
-
-        possibleSemesters.forEach(function(semester, index, array) {
-            var year = currYear; // Make copy of variable so its value is not overwritten by following loops
-
-            var translatedSemester = semesterTranslator[semester];
-            var translatedYear = translatedCurrYear;
-
-            dataValues['schedule_beginterm'] = year.toString() + semester.toString();
-
-            console.log('pinging for ' + translatedYear + ' and ' + translatedSemester);
-            request.post(url, {form:dataValues}, function(err, resp, body) {
-                console.log('response for ' + translatedYear + ' and ' + translatedSemester);
-                if (resp.statusCode == 200) {
-                    var semesterData = parseSemesterData(body);
-                    if (Object.keys(semesterData).length)
-                        scheduleData[translatedYear][translatedSemester] = semesterData;
-                }
-                else {
-                    console.log(resp.statusCode);
-                }
-
-                numResponses++;
-
-                if (numResponses >= numResponsesExpected)
-                    writeDataToFile(scheduleData);
-            });
-        });
-    }
-}
-
-// Filters are things used to narrow down the options when browsing the wave course schedule,
-// such as department, general requirements, etc.
-function fetchAndParseFilterData() {
-    // The url for searching the course schedule
-    var url = 'https://weblprod1.wheatonma.edu/PROD/bzcrschd.P_ListSection';
-
-    // All the filters we care about.
-    var filters = [
-        'subject_sch',
-        'foundation_sch',
-        'division_sch',
-        'area_sch'
-        // 'intmajor_sch' // Currently don't care about interdisciplinary majors
-    ];
-
-    var filterTranslator = {
-        'subject_sch': 'department',
-        'foundation_sch': 'foundation',
-        'division_sch': 'division',
-        'area_sch': 'area'
-        // 'intmajor_sch': 'interdis_major' // Currently don't care about interdisciplinary majors
-    }
-
-    request.get(url, function(err, resp, body) {
-        $ = cheerio.load(body);
-
-        var filtersObject = {};
-
-        addYearsToFilters(filtersObject);
-
-        filters.forEach(function(filter, index, array) {
-            var translatedFilter = filterTranslator[filter];
-            filtersObject[translatedFilter] = [];
-
-            $('select[name=' + filter + ']').find('option').each(function(entry) {
-                var filterValue = $(this).val();
-                if (filterValue != '%')
-                    filtersObject[translatedFilter].push({ val: filterValue, display: filterValue });
-            });
-        });
-
-        fs.readFile('static/course-data/filters.jade', function(err, data) {
-            if (err) console.log(err);
-
-            var func = jade.compile(data, { pretty: debug });
-
-            var html = func({ filterData: filtersObject });
-
-            fs.writeFile('static/course-data/compiled/filters.html', html, function(err) {
-                if (err) console.log(err);
-                else console.log("The filters html file was saved!");
-            });
-
-            if (debug) {
-                fs.writeFile('static/course-data/filters.json', JSON.stringify(filtersObject, null, 2), function(err) {
-                    if (err) console.log(err);
-                    else console.log("The filters json file was saved!");
-                });
-            }
-        });
+    }).catch(function(err) {
+        console.error(err);
+        throw err;
     });
 }
 
-fetchAndParseScheduleData();
-// fetchAndParseFilterData();
+function saveScheduleData() {
+    // Note: Schedule data will be in the global variable, not passed as a parameter
+
+    for (var key in scheduleData) {
+        // This promise will always resolve, and resolve with this key
+        var promise = Promise.resolve(key);
+
+        // fs.writeFile()
+        promise.then(function(key) {
+            return new Promise(function(resolve, reject) {
+                fs.readFile('static/course-data/courses.jade', function(err, data) {
+                    if (err)
+                        reject(err);
+                    else
+                        resolve(data);
+                });
+            }).then(function(template) {
+                var func = jade.compile(template, { pretty: debug });
+                var html = func({ courseData: scheduleData[key] });
+
+                fs.writeFile('static/course-data/compiled/' + key + '.html', html, function(err) {
+                    if (err) console.error(err);
+                    else console.log("The courses " + key + " html file was saved!");
+                });
+
+                if (debug) {
+                    fs.writeFile('static/course-data/' + key + '.json', JSON.stringify(scheduleData[key], null, 2), function(err) {
+                        if (err) console.error(err);
+                        else console.log("The courses " + key + " json file was saved!");
+                    });
+                }
+            }).catch(function(err) {
+                console.error('Error in saving schedule data');
+                console.error(err);
+                throw err;
+            });
+        });
+    }
+}
+
+// =========================== Driver Stuff ===========================
+
+function fetchAndParseAll() {
+    getSearchFilters()
+        .then(preprocessFilters)
+        .then(function(filterObj) {
+            saveFilters(filterObj); // Fire off async call, don't care when it finishes
+
+            if (debug)
+                return filterObj['semester'].slice(0, 7);
+            else
+                return filterObj['semester'];
+        })
+        .then(getScheduleData)
+        .then(saveScheduleData)
+        .catch(function(err) {
+            console.error('Error uncaught elsewhere:');
+            console.error(err);
+            throw err;
+        }
+    ).catch(function(err) {
+        throw err;
+    });
+}
+
+fetchAndParseAll();
