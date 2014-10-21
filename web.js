6673c992a056aac3943aa184bea63bd61b06d472
Bryan Jensen <bryanawjensen@gmail.com>
2014-10-21 19:49:04 -0400

    Cleanup and formatting

--- web.js
+++ web.js
@@ -22,6 +22,16 @@ function shuffle(o) {
 
 var app = express();
 
+
+// Static serving files from specific folders
+app.use('/foundation.custom', express.static(__dirname + '/foundation.custom'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/static', express.static(__dirname + '/static'));
+app.use(logfmt.requestLogger());
+
+
 // Middleware to save the current url, from the request, to the responses' local variables, for use in jade
 app.use(function(req, res, next) {
     res.locals.url = req.url;
@@ -36,10 +46,12 @@ app.use(function(req, res, next) {
     next();
 });
 
-// Special route for the main page, to load the correct jade file
+
+// Special route for the main page
 app.get('/', function(req, res) {
     res.render('main.jade');
 });
+
 
 // Special route for requesting an update to the competitions database
 app.get('/refresh-competitions', function(req, res) {
@@ -55,6 +67,7 @@ app.get('/refresh-competitions', function(req, res) {
     });
     res.end('Success!');
 });
+
 
 app.get('/wave-data', function(req, res) {
     var year = req.query.year || '2014-2015';
@@ -72,10 +85,11 @@ app.get('/wave-data', function(req, res) {
     });
 });
 
-// Special route for everything to do with WAVE
+
 app.get('/wave', function(req, res) {
     res.render('wave.jade', { year: req.query.year || '2014-2015' });
 });
+
 
 app.get('/members', function(req, res) {
     fs.readFile('static/member-data/members.json', function renderPageWithJSON(err, json) {
@@ -84,44 +98,30 @@ app.get('/members', function(req, res) {
             res.render('404.jade');
         }
         else {
-            console.log(json);
             res.render('members.jade', { members: shuffle(JSON.parse(json)) }); // Shuffled (for production)
             // res.render('members.jade', { members: JSON.parse(json) }); // Unshuffled (for debugging)
         }
     });
 });
 
+
 // General route for any pages that're static/fully front-end, and just need their jade file parsed and served
 app.get('/:route', function(req, res) {
     console.log('Request at: ' + req.params.route);
     var requestRoute = req.params.route,
-        rerouted = routeMap[requestRoute];
+        rerouted = routeMap[requestRoute] || '404';
 
-    if (rerouted == undefined) {
-        rerouted = '404';
-    }
-    
     res.render(rerouted + '.jade');
 });
 
-// Configure the static folders that're ok to serve to anyone who asks (a.k.a. most browsers looking for javascript files, etc.)
-app.use(logfmt.requestLogger());
 
-// Static serving files from specific folders
-app.use('/foundation.custom', express.static(__dirname + '/foundation.custom'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js', express.static(__dirname + '/js'));
-app.use('/images', express.static(__dirname + '/images'));
-app.use('/static', express.static(__dirname + '/static'));
+app.get('*', function(req, res){
+    res.render('404.jade');
+});
 
-// app.all('*', function(req, res){
-//     res.render('404.jade');
-// });
 
 // Server on port 7500
 var port = 7500;
-if (process.argv[2] != 'undefined' && process.argv[2] != undefined) port = process.argv[2];
-
 // Start up the server
 app.listen(port, function() {
     console.log("Listening on " + port);
